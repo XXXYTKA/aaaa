04702d68686c147b8675712526b6272f7a0415b9
XXXYTKA <167037176+XXXYTKA@users.noreply.github.com>
2024-09-30 01:00:08 +0300

    Add files via upload

---
+++ rd-02-04.py
@@ -0,0 +1,14 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+result = get_matrix(3, 4, 5)
+print(result)
+
+
+
+
